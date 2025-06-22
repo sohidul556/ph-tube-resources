bac3aa3362efab94007282886f88b3560814ee8b
Md Sohidul Islam <shamim5567@gmail.com>
2025-06-23 01:46:05 +0600

    api fetch

--- script/index.js
+++ script/index.js
@@ -9,6 +9,20 @@ function loadCategories() {
         .then((data) => displayCategories(data.categories));
 }
 
+
+function loadVideos(){
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+         .then((res) => res.json())
+        // 3 - Send data to display
+        .then((data) => displayVideos(data.videos));
+}
+
+
+
+const loadCategooryVideo = (id)=>{
+    console.log(id);
+}
+
 function displayCategories(categories) {
     // Get the container
     const categoryContainer = document.getElementById("category-container");
@@ -20,15 +34,73 @@ function displayCategories(categories) {
 
         // Create element
         const categoryDiv= document.createElement("div");
-        categoryDiv.innerHTML = `<button class="btn btn-sm hover:bg-red-500 hover:text-white">${cat.category}</button>`;
+        categoryDiv.innerHTML = `<button onclick="loadCategooryVideo()" class="btn btn-sm hover:bg-red-500 hover:text-white">${cat.category}</button>`;
 
         // Append the element
         categoryContainer.appendChild(categoryDiv);
     }
 }
 
-loadCategories();
+const displayVideos=(videos)=>{
+    console.log(videos);
+      
+    const videoContainer= document.getElementById("video-container");
+    videos.forEach((video) => {
+        console.log(video);
+        const videoCard = document.createElement("div");
 
+        videoCard.innerHTML=`<div class="card bg-base-100">
+
+    <figure class="relative"><img  class="w-full h-[150px] object-cover" src="${video.thumbnail}"/>
+    <span class="absolute bottom-2 right-2 text-white bg-black text-sm">3hrs 56 min ago</span>
+    </figure>
+
+     <div class=" flex gap-3 px-0 py-5"> 
+      <div class="profile">
+        <div class="avatar">
+  <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring-2 ring-offset-2">
+    <img src="${video.authors[0].profile_picture}" />
+  </div>
+</div>
+      </div>
+      <div class="intro">
+        <h2 class="text-sm font-semibold">${video.title}</h2>
+        <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name}<img class="w-5 h-5" src=" https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt=""></p>
+        <p class="text-sm text-gray-400">${video.others.views}</p>
+      </div>
+
+  
+      </div>
+  </div>`;
+        //append
+        videoContainer.append(videoCard);
+    });
+}
+
+loadCategories();
+// loadVideos();
+
+// {
+//     "category_id": "1001",
+//     "video_id": "aaaa",
+//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//     "title": "Shape of You",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//             "profile_name": "Olivia Mitchell",
+//             "verified": ""
+//         }
+//     ],
+//     "others": {
+//         "views": "100K",
+//         "posted_date": "16278"
+//     },
+//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// }
+
+
+//  Two are function is right.
 
 // function loadCategories() {
 //     // 1- fetch the data
@@ -42,7 +114,7 @@ loadCategories();
 // function displayCategories(categories) {
 //     console.log(categories)
 //     // get the container
-//   //  const categoryContainer = document.getElementById("category-container");
+//     const categoryContainer = document.getElementById("category-container");
     
 //     // loop through each category in the array of object
 //     categories.forEach(cat => {
